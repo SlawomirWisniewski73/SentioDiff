0c64b3486b4baa92685de0e6addb84aa5990fef4
Sławomir Wiśniewski <144655582+SlawomirWisniewski73@users.noreply.github.com>
2025-07-17 12:41:47 +0200

    Update app.js

--- examples/viewer-demo/app.js
+++ examples/viewer-demo/app.js
@@ -1,1 +1,30 @@
+'use strict';
+
+// Przyk\u0142adowe dane, kt\u00f3re chcemy zwizualizowa\u0107 (dw\u00f3ch tracks)
+const sampleTracks = [
+  { id: 't1', data: [1, 2, 3] },
+  { id: 't2', data: [4, 5, 6] }
+];
+
+// Jeśli biblioteka SentioDiff by\u0142aby dost\u0119pna globalnie (np. zaimportowana jako UMD),
+// mogliby\u015bmy u\u017cy\u0107 ReactTrackView. W zast\u0119pstwie definiujemy prosty komponent:
+function TrackList({ tracks }) {
+  return React.createElement(
+    'ul',
+    null,
+    tracks.map(track =>
+      React.createElement(
+        'li',
+        { key: track.id },
+        track.id + ': ' + track.data.join(', ')
+      )
+    )
+  );
+}
+
+// Wyrenderowanie komponentu TrackList do strony
+const container = document.getElementById('app');
+ReactDOM.createRoot(container).render(
+  React.createElement(TrackList, { tracks: sampleTracks })
+);
 
