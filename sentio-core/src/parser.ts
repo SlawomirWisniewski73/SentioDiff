a6a5a9c1bd18b36e55af7b12c42161a594a363e0
Sławomir Wiśniewski <144655582+SlawomirWisniewski73@users.noreply.github.com>
2025-07-17 21:18:47 +0200

    Update parser.ts

--- sentio-core/src/parser.ts
+++ sentio-core/src/parser.ts
@@ -1,6 +1,6 @@
-import { SentioDoc } from "./types";
+import { SentioData } from './types';
 
-export function parseSentio(json: any): SentioData {
+export function parse(json: unknown): SentioData {
   if (!json.version || !json.entities) {
     throw new Error("Invalid SentioDiff document: missing required fields.");
   }
