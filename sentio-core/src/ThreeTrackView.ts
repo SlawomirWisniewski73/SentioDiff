5cac42dd79fb19ab74694885922c4f54957261a6
Sławomir Wiśniewski <144655582+SlawomirWisniewski73@users.noreply.github.com>
2025-07-17 19:38:21 +0200

    Create ThreeTrackView.ts

---
+++ sentio-core/src/ThreeTrackView.ts
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+
+export class ThreeTrackView {
+  private scene: THREE.Scene;
+
+  constructor() {
+    this.scene = new THREE.Scene();
+  }
+
+  render(container: HTMLElement) {
+    const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    container.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    this.scene.add(cube);
+
+    camera.position.z = 5;
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(this.scene, camera);
+    };
+    animate();
+  }
+}
